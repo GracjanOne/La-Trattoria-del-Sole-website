a0b5c71399642a2db1084988ece2aebcb8b5af2e
unknown <gracjan.zalewski55@gmail.com>
2023-06-29 19:59:33 +0200

    ADD ABOUT US SECTION, OPINION SECTION AND CONTACT

---
+++ src/script.js
@@ -0,0 +1,20 @@
+const landing_page_button = document.querySelector("#landing-page-button");
+const send_button = document.querySelector("#send-button");
+
+landing_page_button.addEventListener("mousedown", function(){
+    landing_page_button.style.boxShadow = "inset 0px 0px 7px 0px rgba(66, 68, 90, 1)";
+});
+landing_page_button.addEventListener("mouseup", function(){
+    landing_page_button.style.boxShadow = "none";
+});
+send_button.addEventListener("mousedown", function(){
+    send_button.style.boxShadow = "inset -1px 2px 4px 6px rgba(0, 0, 0, 0.25)";
+});
+send_button.addEventListener("mouseup", function(){
+    send_button.style.boxShadow = "-6px 4px 4px 6px rgba(0, 0, 0, 0.25)";
+    window.setTimeout(msgSend,500);
+});
+function msgSend(){
+    alert("Twoja wiadomość została poprawnie wysłana!");
+};
+
