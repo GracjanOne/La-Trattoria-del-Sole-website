0a6c7a7676bd8bc6ca42b4513cb7d81dd17ba943
unknown <gracjan.zalewski55@gmail.com>
2023-07-31 20:06:18 +0200

    FULLY RESPONSIVE

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 const landing_page_button = document.querySelector("#landing-page-button");
 const send_button = document.querySelector("#send-button");
-
+const hamburgerMenu = document.querySelector("#hamburger-menu"); 
+const sidebar = document.querySelector("#right-menu");
+const closeSidebar = document.querySelector("#close-sidebar");
 landing_page_button.addEventListener("mousedown", function(){
     landing_page_button.style.boxShadow = "inset 0px 0px 7px 0px rgba(66, 68, 90, 1)";
 });
@@ -12,10 +14,17 @@ send_button.addEventListener("mousedown", function(){
 });
 send_button.addEventListener("mouseup", function(){
     send_button.style.boxShadow = "-6px 4px 4px 6px rgba(0, 0, 0, 0.25)";
-    //TODO dodaj usuwanie danych po wysłaniu i zrób warunek że email nie może być pusty
     window.setTimeout(msgSend,500);
 });
 function msgSend(){
     alert("Twoja wiadomość została poprawnie wysłana!");
 };
 
+hamburgerMenu.addEventListener("click", () =>{
+    sidebar.style.display = "block"
+    //Dodaj animacje
+})
+closeSidebar.addEventListener("click", (evt) =>{
+    evt.preventDefault();
+    sidebar.style.display = "none"
+})
