94b46b9216ebbfcf620170f095b5b4e93b0021ef
unknown <gracjan.zalewski55@gmail.com>
2023-08-01 19:45:05 +0200

    ADD ONSCROLL ANIMATION

--- src/script.js
+++ src/script.js
@@ -1,30 +1,36 @@
 const landing_page_button = document.querySelector("#landing-page-button");
 const send_button = document.querySelector("#send-button");
-const hamburgerMenu = document.querySelector("#hamburger-menu"); 
+const hamburgerMenu = document.querySelector("#hamburger-menu");
 const sidebar = document.querySelector("#right-menu");
 const closeSidebar = document.querySelector("#close-sidebar");
-landing_page_button.addEventListener("mousedown", function(){
-    landing_page_button.style.boxShadow = "inset 0px 0px 7px 0px rgba(66, 68, 90, 1)";
+const pizza = document.querySelector("#phone-pizza-background");
+landing_page_button.addEventListener("mousedown", function () {
+  landing_page_button.style.boxShadow =
+    "inset 0px 0px 7px 0px rgba(66, 68, 90, 1)";
 });
-landing_page_button.addEventListener("mouseup", function(){
-    landing_page_button.style.boxShadow = "none";
+landing_page_button.addEventListener("mouseup", function () {
+  landing_page_button.style.boxShadow = "none";
 });
-send_button.addEventListener("mousedown", function(){
-    send_button.style.boxShadow = "inset -1px 2px 4px 6px rgba(0, 0, 0, 0.25)";
+send_button.addEventListener("mousedown", function () {
+  send_button.style.boxShadow = "inset -1px 2px 4px 6px rgba(0, 0, 0, 0.25)";
 });
-send_button.addEventListener("mouseup", function(){
-    send_button.style.boxShadow = "-6px 4px 4px 6px rgba(0, 0, 0, 0.25)";
-    window.setTimeout(msgSend,500);
+send_button.addEventListener("mouseup", function () {
+  send_button.style.boxShadow = "-6px 4px 4px 6px rgba(0, 0, 0, 0.25)";
+  window.setTimeout(msgSend, 500);
 });
-function msgSend(){
-    alert("Twoja wiadomość została poprawnie wysłana!");
-};
+function msgSend() {
+  alert("Twoja wiadomość została poprawnie wysłana!");
+}
 
-hamburgerMenu.addEventListener("click", () =>{
-    sidebar.style.display = "block"
-    //Dodaj animacje
-})
-closeSidebar.addEventListener("click", (evt) =>{
-    evt.preventDefault();
-    sidebar.style.display = "none"
-})
+hamburgerMenu.addEventListener("click", () => {
+  sidebar.style.display = "block";
+  //Dodaj animacje
+});
+closeSidebar.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  sidebar.style.display = "none";
+});
+
+onscroll = (evt) => {
+  pizza.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
+};
